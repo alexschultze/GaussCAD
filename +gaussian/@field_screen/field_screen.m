7d4cf1cef2897903915fcaa186edc9ac75fa101c
Alexander Schultze <alexschultze@gmail.com>
2021-10-26 11:36:16 +0200

    -fix intensity/irradiance -fix code intentation

--- +gaussian/@field_screen/field_screen.m
+++ +gaussian/@field_screen/field_screen.m
@@ -1,6 +1,6 @@
 classdef field_screen < handle & matlab.mixin.Copyable
     %FIELD SCREEN is a class for projecting complex
-    % wave fields. It can be used to represent 
+    % wave fields. It can be used to represent
     % complex field sensors such as single element photodiodes
     % for interferometry.
     % A. Schultze 01/10/2020 (GaussCAD toolbox)
@@ -65,9 +65,9 @@ classdef field_screen < handle & matlab.mixin.Copyable
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
             [ yy,zz] = meshgrid( y, z );
             
-% REAR   View FRONT       (z)
-% 3 | 4       4 | 3        ^
-% 2 | 1       1 | 2        |-> (y)
+            % REAR   View FRONT       (z)
+            % 3 | 4       4 | 3        ^
+            % 2 | 1       1 | 2        |-> (y)
             switch (a_q)
                 case 1
                     qq = (yy<0) & (zz < 0); %-y -z
@@ -79,44 +79,43 @@ classdef field_screen < handle & matlab.mixin.Copyable
                     qq = (yy<0) & (zz > 0); %-y +z
                 otherwise
                     warning('No valid quadrant specified');
-            end 
+            end
             retval = qq;
         end
         
-
         
-        %Calculate a single value response for intensity and phase for 
+        
+        %Calculate a single value response for intensity and phase for
         %a masked segment
         function [int,phase,e] = calc_int_phase_masked(self,par_mask)
-           
-            %ac amplitude: min(abs(E1),abs(E2))
-            %dc amplitude: max(abs(E1),abs(E2))- ac
-            ac = min(abs(self.E(:,:,1)),abs(self.E(:,:,2)));
-            %dc = max(abs(self.E(:,:,1)),abs(self.E(:,:,2)))-ac;
             
+            %Superposition of E1 and E2
+            E_m = (self.E(:,:,1)+self.E(:,:,2)).*par_mask;
             
-            %phase_angle: angle(interferogram)
-            ph_angle =  angle(self.E);
-            ph_angle =  ph_angle(:,:,1)-ph_angle(:,:,2);   %Interferogram 
-           %ph_angle = ph_angle-ph_angle(end/2, end/2);   %Center to mid
+            % Local pixel intensity (irradiance)
+            I =E_m.*conj(E_m); %per pixel
+            int = mean(I,'all');
 
+            % Local pixel contrast, for phase intensity contribution
+            ac = min(abs(self.E(:,:,1)),abs(self.E(:,:,2)));
+            
+            ph_angle =  angle(self.E);          
+            ph_angle =  ph_angle(:,:,1)-ph_angle(:,:,2);   %Interferogram 
+ 
             if max(ph_angle,[],'all')-min(ph_angle,[],'all') > pi
                 %This is slow. Only when necessary.
                 ph_angle = lib.unwrap_2d.phase_unwrap_TV_min(ph_angle,'no');
             end
-            
-            %Complex Weighed Phase Angle Vector Field
+    
             ph_angle_weighed = ac.*exp(-1i*ph_angle);
 
-            %momentary intensity "irradiance"
-            int=abs(sum(self.E.*(par_mask),'all')); 
-            %phase (weighed)
-            phase = angle(sum(ph_angle_weighed.*(par_mask),'all'));  
-            %complex field response
+            phase = angle(sum(ph_angle_weighed.*(par_mask),'all'));
+            
+            
             e  = sum(self.E.*(par_mask),'all');
         end
         
-       
+        
         %Complete (masked) intensity and phase
         function [int,phase,e] = calc_int_phase(self)
             [int,phase,e] = self.calc_int_phase_masked(self.mask);
@@ -127,11 +126,11 @@ classdef field_screen < handle & matlab.mixin.Copyable
             int=zeros(1,4);
             phase=zeros(1,4);
             e=zeros(1,4);
-                
-             for i=1:4
+            
+            for i=1:4
                 this_mask = (self.mask_quadrant(i) & self.mask);
                 [int(i),phase(i),e(i)] = self.calc_int_phase_masked(this_mask);
-            end 
+            end
         end
         
         function [dws_y,dws_z] = calc_dws(self)
@@ -141,7 +140,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
             % REAR   View FRONT       (z)
             % 3 | 4       4 | 3        ^
             % 2 | 1       1 | 2        |-> (y)
-    
+            
             [~,ph,e] = calc_int_phase_quadrants(self);
             dws_y =  (ph(2)+ph(3))-(ph(1)+ph(4));
             dws_z =  (ph(3)+ph(4))-(ph(1)+ph(2));
@@ -154,7 +153,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
         function [ac,dc] = plot_contrast(self, axes_h)
             ac = min(abs(self.E(:,:,1)),abs(self.E(:,:,2)));
             dc = max(abs(self.E(:,:,1)),abs(self.E(:,:,2)))-ac;
-             
+            
             y = linspace( -self.dim(1)/2, self.dim(1)/2, self.pix(1) );
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
             [ xx,yy ] = meshgrid( y,z );
@@ -169,7 +168,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
                 xlabel( 'Sensor Y (mm)');ylabel( 'Sensor Z (mm)');
                 subplot(2,1,2);
                 surf(xx,yy,dc,'EdgeColor','none'); title('DC');
-                xlabel('Sensor Y (mm)');ylabel('Sensor Z (mm)'); 
+                xlabel('Sensor Y (mm)');ylabel('Sensor Z (mm)');
             end
         end
         
@@ -177,7 +176,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
         function [ac,dc] = plot_contrast_ac_dc(self)
             ac = min(abs(self.E(:,:,1)),abs(self.E(:,:,2)));
             dc = max(abs(self.E(:,:,1)),abs(self.E(:,:,2)))-ac;
-             
+            
             y = linspace( -self.dim(1)/2, self.dim(1)/2, self.pix(1) );
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
             [ xx,yy ] = meshgrid( y,z );
@@ -186,7 +185,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
             xlabel('Sensor Y (mm)');ylabel('Sensor Z (mm)');
             subplot(2,1,2);
             surf(xx,yy,dc,'EdgeColor','none'); title('DC');
-            xlabel('Sensor Y (mm)');ylabel('Sensor Z (mm)'); 
+            xlabel('Sensor Y (mm)');ylabel('Sensor Z (mm)');
         end
         
         function [ret_contrast,ret_ac,ret_dc] = calc_contrast(self)
@@ -196,39 +195,39 @@ classdef field_screen < handle & matlab.mixin.Copyable
         function [ret_ac,ret_dc, ret_contrast] = calc_contrast_masked(self, par_mask)
             ac = 2*min(abs(self.E(:,:,1)),abs(self.E(:,:,2)));
             dc = max(abs(self.E(:,:,1)),abs(self.E(:,:,2)))-ac/2;
- 
+            
             ret_ac = sum(ac.*par_mask,'all');
             ret_dc = sum(dc.*par_mask,'all');
             ret_contrast= ret_ac/(ret_ac+ret_dc);
         end
         
-          
+        
         
         function retval = plot_mask(self)
             y = linspace( -self.dim(1)/2, self.dim(1)/2, self.pix(1) );
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
             [ yy,zz] = meshgrid( y, z );
             
-
-           subplot(2,2,1);
-           retval=surf(yy,zz,double(self.mask),'EdgeColor','none'); 
-           
+            
+            subplot(2,2,1);
+            retval=surf(yy,zz,double(self.mask),'EdgeColor','none');
+            
             xlabel('y');ylabel('z');
             view([0 90]);
             for i=1:3
-               subplot(2,2,1+i); 
-               surf(yy,zz,double(self.mask & self.mask_quadrant(i)),'EdgeColor','none');
-               xlabel('y');ylabel('z');
-               legend(['Q' num2str(i)],'Location','southeast');
-               view([0 90]);
-            end   
+                subplot(2,2,1+i);
+                surf(yy,zz,double(self.mask & self.mask_quadrant(i)),'EdgeColor','none');
+                xlabel('y');ylabel('z');
+                legend(['Q' num2str(i)],'Location','southeast');
+                view([0 90]);
+            end
             suptitle('Screen Mask');
         end
         
         function retval = render(self)
             % Pixels and their position in space
             % Without rotation Reference Y = Screen Z ("up");
-
+            
             y = linspace( -self.dim(1)/2, self.dim(1)/2, self.pix(1) );
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
             x = 0;
@@ -239,21 +238,21 @@ classdef field_screen < handle & matlab.mixin.Copyable
             % rotate and shift
             if self.rotang ~= 0
                 S = rodrigues_rot( S, self.rotax, self.rotang );
-            end     
-            %calculate the E field for each position for each beam           
+            end
+            %calculate the E field for each position for each beam
             for i=1:length(self.beams)
                 this_beam=self.beams(i);
                 this_E=this_beam.calc_field_xyz( S + self.r);
                 self.E(:,:,i)= squeeze(reshape(this_E,size(xx)))';
             end
-            retval= self.E;                      
+            retval= self.E;
         end % function retval = render(self)
         
         %This function will simplify and speed up
         %shifting phase of one beams rendered result
         %Useful for fast plotting
         function shift_phase_beam1(self,a_phi)
-            self.E(:,:,1)=exp(-1i*a_phi).*self.E(:,:,1);    
+            self.E(:,:,1)=exp(-1i*a_phi).*self.E(:,:,1);
         end
         
         function ret_handle = plot_intensity(self)
@@ -267,25 +266,25 @@ classdef field_screen < handle & matlab.mixin.Copyable
             
             for i=1:size(self.E,3)
                 subplot(3,1,1+i);
-                ret_handle=surf(xx,yy,abs(self.E(:,:,i)),'EdgeColor','none');hold on;
+                ret_handle=surf(xx,yy,self.E(:,:,i).*conj(self.E(:,:,i)),'EdgeColor','none');hold on;
                 title(['Beam #' num2str(i)]);
             end
-
-
-                       
+            
+            
+            
         end %plot intensity
         
         
-         function ret_handle = plot_interference_int(self, axes_h)
-             
+        function ret_handle = plot_interference_int(self, axes_h)
             
-             
+            
+            
             y = linspace( -self.dim(1)/2, self.dim(1)/2, self.pix(1) );
             z = linspace( -self.dim(2)/2, self.dim(2)/2, self.pix(2) );
-            [ xx,yy ] = meshgrid( y,z ); 
+            [ xx,yy ] = meshgrid( y,z );
             
-                        %max possible intensity for scaling
-            max_int = max(sum(abs(self.E),3).^2,[],'all'); 
+            %max possible intensity for scaling
+            max_int = max(sum(abs(self.E),3).^2,[],'all');
             
             ret_handle=surf(axes_h,xx,yy,abs(sum(self.E,3)).^2,'EdgeColor','none');
             %slightly faster alternative
@@ -295,7 +294,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
             xlabel(axes_h,'Sensor Y (mm)');ylabel(axes_h,'Sensor Z (mm)');
             title(axes_h,'Interference (Intensity)');
             
-         end
+        end
         
         
         function ret_handle = plot_interference(self)
@@ -305,10 +304,10 @@ classdef field_screen < handle & matlab.mixin.Copyable
             subplot(2,1,1);
             
             %max possible intensity for scaling
-            max_int = max(sum(abs(self.E),3).^2,[],'all'); 
+            max_int = max(sum(abs(self.E),3).^2,[],'all');
             
             
-
+            
             ret_handle=surf(xx,yy,abs(sum(self.E,3)).^2,'EdgeColor','none');
             %slightly faster alternative
             %image(abs(sum(self.E,3)).^2,'CDataMapping','scaled');
@@ -318,7 +317,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
             title('Interference (Intensity)');
             
             ph_angle =  angle(self.E);
-            phase =  ph_angle(:,:,1)-ph_angle(:,:,2);         
+            phase =  ph_angle(:,:,1)-ph_angle(:,:,2);
             phase = lib.unwrap_2d.phase_unwrap_TV_min(phase,'no');
             phase = phase-phase(end/2,end/2); %center mid to 0
             
@@ -326,9 +325,9 @@ classdef field_screen < handle & matlab.mixin.Copyable
             if ~exist('axis_handle','var')
                 subplot(2,1,2)
                 ret_handle=surf(xx,yy,phase,'EdgeColor','none');
-                        %slightly faster alternative
+                %slightly faster alternative
                 %image(phase,'CDataMapping','scaled');
-                            zlabel('Phase (rad)');
+                zlabel('Phase (rad)');
                 title('Interference (Phase)');
                 %zlim([-1 1]*pi);
                 colorbar;caxis([-1 1]*pi);view([0,90]);
@@ -337,7 +336,7 @@ classdef field_screen < handle & matlab.mixin.Copyable
             
             
             
-    
+            
         end % plot interference
         
     end
